9cb0bbcecad36b93c2566ff33326a88968033a16
ppvyctor <pedro.vyctor00@gmail.com>
2025-11-24 20:23:23 -0300

    Adding the game exe

--- Source/src/Website.py
+++ Source/src/Website.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import os
+import zipfile
+
 
 st.set_page_config(page_title="Business Game", page_icon="🎮")
 
@@ -10,6 +12,16 @@ st.markdown(
 )
 
 st.markdown("<br><br><br><br>", unsafe_allow_html=True)
+
+'''def create_zip():
+    # Cria um arquivo zip a partir do arquivo rar
+    with zipfile.ZipFile(r'Game\Business_Game.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk(source_folder):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, source_folder)
+                zipf.write(file_path, arcname)
+'''
 
 #left, right = st.columns(2)
 
@@ -23,7 +35,7 @@ caminho_arquivo = os.path.join(diretorio_script, "Business_Game.rar")
 st.markdown('<h1>Windows</h1>', unsafe_allow_html=True)
 
 with open("downloads\\Business_Game.rar", "r") as f:
-    st.download_button("Business Game.rar", f.read(), file_name="BusinessGame.rar", mime="application/x-rar-compressed")
+    st.download_button("Business Game.rar", f.read(), file_name="BusinessGame.rar", mime="application/x-rar-compressed", on_click = create_zip)
 
 with open("downloads\\Business_Game.zip", "rb") as f:
     st.download_button("Business Game.zip", f.read(), file_name="BusinessGame.zip", mime="application/x-zip-compressed")
